36faf7ad374c86a4a7ab08c2fbaa7a98216b9ca3
Oleksii Butrin <bootreen@gmail.com>
2024-07-05 15:31:34 +0200

    Added basic project structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
 // import React from 'react'
 import "../css/styles.css";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./routes/RootLayout.jsx";
+import Home from "./routes/Home.jsx";
+import About from "./routes/About.jsx";
+import Projects from "./routes/Projects.jsx";
+import Contact from "./routes/Contact.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/projects",
+        element: <Projects />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <App />
+  <RouterProvider router={router} />
   // </React.StrictMode>,
 );
--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,9 +1,9 @@
-const App = () => {
+const Contact = () => {
   return (
     <article className='mt-8 prose prose-slate mx-auto lg:prose-xl'>
-      <h1>Portfolio</h1>
+      <h2>Contact</h2>
     </article>
   );
 };
 
-export default App;
+export default Contact;
