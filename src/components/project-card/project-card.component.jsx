025d94a5963dd3a4c9fe4cda23b508a1662679d8
Oleksii Butrin <bootreen@gmail.com>
2024-07-08 21:51:43 +0200

    Moved project content mapping into Portfolio route

--- src/components/project-card/project-card.component.jsx
+++ src/components/project-card/project-card.component.jsx
@@ -9,42 +9,41 @@ import {
   styleProjectCardButtonsContainer,
   styleNavButton,
 } from "./project-card.style";
-import { content } from "../../store/content";
-import { usePortfolioStore } from "../../store/store";
 
-const ProjectCard = () => {
-  const locale = usePortfolioStore(({ activeLanguage }) => activeLanguage);
-  const { projects } = content;
-  return projects[locale].map(
-    ({ title, thumbnail, features, description, buttons }, index) => (
-      <div key={index} className={styleProjectCardContainer}>
-        <input type='radio' name='projects' defaultChecked={index === 0} />
-        <div className={styleProjectCardPreview}>
-          <img className={styleProjectCardThumbnail} src={thumbnail} />
-          <div className={styleProjectCardInfoContainer}>
-            <h3 className={styleProjectCardTitle}>{title}</h3>
-            <ul className={styleProjectCardFeatures}>
-              {features.map((feature, index) => (
-                <li key={index}>{feature}</li>
-              ))}
-            </ul>
-          </div>
-        </div>
-        <div className={styleProjectCardDetails}>
-          {description.map((paragraph, index) => (
-            <p key={index}>{paragraph}</p>
+const ProjectCard = ({
+  title,
+  thumbnail,
+  features,
+  description,
+  buttons,
+  isChecked,
+}) => (
+  <div className={styleProjectCardContainer}>
+    <input type='radio' name='projects' defaultChecked={isChecked} />
+    <div className={styleProjectCardPreview}>
+      <img className={styleProjectCardThumbnail} src={thumbnail} />
+      <div className={styleProjectCardInfoContainer}>
+        <h3 className={styleProjectCardTitle}>{title}</h3>
+        <ul className={styleProjectCardFeatures}>
+          {features.map((feature, index) => (
+            <li key={index}>{feature}</li>
           ))}
-          <div className={styleProjectCardButtonsContainer}>
-            {buttons.map(({ caption, url }, index) => (
-              <a key={index} className={styleNavButton} href={url}>
-                {caption}
-              </a>
-            ))}
-          </div>
-        </div>
+        </ul>
       </div>
-    )
-  );
-};
+    </div>
+    <div className={styleProjectCardDetails}>
+      {description.map((paragraph, index) => (
+        <p key={index}>{paragraph}</p>
+      ))}
+      <div className={styleProjectCardButtonsContainer}>
+        {buttons.map(({ caption, url }, index) => (
+          <a key={index} className={styleNavButton} href={url}>
+            {caption}
+          </a>
+        ))}
+      </div>
+    </div>
+  </div>
+);
 
 export default ProjectCard;
--- src/routes/portfolio/portfolio.route.jsx
+++ src/routes/portfolio/portfolio.route.jsx
@@ -1,10 +1,26 @@
 import { styleProjectsContainer } from "./portfolio.style";
 import ProjectCard from "../../components/project-card/project-card.component";
+import { content } from "../../store/content";
+import { usePortfolioStore } from "../../store/store";
 
 const Portfolio = () => {
+  const locale = usePortfolioStore(({ activeLanguage }) => activeLanguage);
+  const { projects } = content;
   return (
     <div className={styleProjectsContainer}>
-      <ProjectCard />
+      {projects[locale].map(
+        ({ title, thumbnail, features, description, buttons }, index) => (
+          <ProjectCard
+            key={index}
+            title={title}
+            thumbnail={thumbnail}
+            features={features}
+            description={description}
+            buttons={buttons}
+            isChecked={index === 0}
+          />
+        )
+      )}
 
       {/* <div className='collapse collapse-arrow bg-base-100 mb-4 rounded-lg'>
         <input type='radio' name='my-accordion-2' defaultChecked />
