7f3058b034b51295c699b8682a12727740310ecf
Oleksii Butrin <bootreen@gmail.com>
2024-07-08 11:22:52 +0200

    Separated route and component for About section

---
+++ src/components/intro/intro.component.jsx
@@ -0,0 +1,35 @@
+import {
+  descriptionContainer,
+  descriptionContent,
+  descriptionHeading,
+  descriptionParagraph,
+  photoSquare,
+  photoVertical,
+} from "./intro.style";
+import { content } from "../../store/content";
+import { usePortfolioStore } from "../../store/store";
+
+const Intro = () => {
+  const locale = usePortfolioStore(({ activeLanguage }) => activeLanguage);
+  const {
+    about: { heading, description },
+  } = content;
+  return (
+    <div className={descriptionContainer}>
+      <div className={descriptionContent}>
+        <img src='/photo-sq.jpg' className={photoSquare} />
+        <img src='/photo-ver.jpg' className={photoVertical} />
+        <div>
+          <h1 className={descriptionHeading}>{heading[locale]}</h1>
+          {description[locale].map((paragraph, index) => (
+            <p key={index} className={descriptionParagraph}>
+              {paragraph}
+            </p>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
