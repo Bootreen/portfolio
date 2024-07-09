037c5dd73ffee3083448ecf6762bc0fd1a42fab7
Oleksii Butrin <bootreen@gmail.com>
2024-07-09 12:34:46 +0200

    Fixed accordion marker layout bug

--- src/components/project-card/project-card.style.js
+++ src/components/project-card/project-card.style.js
@@ -1,12 +1,12 @@
 export const styleProjectCardContainer =
   "h-fit w-11/12 sm:w-[38rem] mx-4 sm:mx-0 collapse collapse-arrow bg-base-100 rounded-lg";
 export const styleProjectCardPreview =
-  "collapse-title w-max text-xl font-medium flex flex-row justify-center sm:justify-start";
+  "collapse-title text-xl font-medium flex flex-row justify-center sm:justify-start";
 export const styleProjectCardThumbnail =
   "hidden sm:block w-48 h-32 rounded-md m-4";
 export const styleProjectCardImage =
   "sm:hidden w-full h-auto rounded-md m-auto";
-export const styleProjectCardInfoContainer = "h-full p-2 flex flex-col";
+export const styleProjectCardInfoContainer = "h-full w-full p-2 flex flex-col";
 export const styleProjectCardTitle =
   "pl-5 pb-2 text-2xl text-center sm:text-left text-secondary font-bold";
 export const styleProjectCardFeatures = "pl-5 list-disc text-base ";
