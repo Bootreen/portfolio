76bddcf1ee19b247047ef6aa62508a1160f7f35f
Oleksii Butrin <bootreen@gmail.com>
2024-08-04 21:13:26 +0200

    Removed hover for mobile

--- src/components/timeline-item/timeline-item.style.js
+++ src/components/timeline-item/timeline-item.style.js
@@ -4,7 +4,7 @@ const style = {
   timelineDate: "timeline-start mr-4 text-sm md:text-lg text-base-content",
   timelineElement:
     "timeline-end timeline-box w-fit ml-4 my-0 md:my-2 p-0 border-none shadow-none rounded-xl bg-base-300",
-  timelineExpandable: "collapse hover:text-secondary",
+  timelineExpandable: "collapse lg:hover:text-secondary",
   timelineExpandableTitle:
     "m-0 md:ml-4 collapse-title text-md md:text-xl font-medium w-fit",
   timelineExpandableContent: "collapse-content max-w-lg lg:max-w-2xl",
--- src/routes/contact/contact.style.js
+++ src/routes/contact/contact.style.js
@@ -1,8 +1,8 @@
 const style = {
   contactContainer:
-    "min-h-screen min-w-full px-4 py-14 md:py-16 lg:py-20 flex justify-center text-content bg-base-300 prose prose-slate lg:prose-xl",
+    "min-h-screen min-w-full px-4 py-14 md:py-16 lg:py-20 flex justify-center text-xl text-content bg-base-300 prose prose-slate lg:prose-xl",
   listElement: "text-content",
-  link: "no-underline hover:text-secondary",
+  link: "no-underline lg:hover:text-secondary",
 };
 
 export default style;
