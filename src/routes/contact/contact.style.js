edf58914c893f387e9e6533ada0822b9d2dad21b
Oleksii Butrin <bootreen@gmail.com>
2024-08-04 21:06:53 +0200

    Added contact section

--- src/routes/contact/contact.style.js
+++ src/routes/contact/contact.style.js
@@ -1,6 +1,8 @@
 const style = {
   contactContainer:
-    "min-h-screen min-w-full mx-auto px-4 py-14 md:py-16 lg:py-20 bg-base-200 prose prose-slate lg:prose-xl prose-headings:text-center",
+    "min-h-screen min-w-full px-4 py-14 md:py-16 lg:py-20 flex justify-center text-content bg-base-300 prose prose-slate lg:prose-xl",
+  listElement: "text-content",
+  link: "no-underline hover:text-secondary",
 };
 
 export default style;
